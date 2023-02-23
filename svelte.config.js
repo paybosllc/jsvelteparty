671e2695c82d91743ddb8986f680dcc9c77b27a0
jeffl <jeffl@hp.com>
2023-02-22 22:08:24 -0800

    vite preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	// preprocess: vitePreprocess(),
-	preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
