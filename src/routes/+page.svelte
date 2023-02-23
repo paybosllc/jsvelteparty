65131290f9ce6b39edd01319232d6ca2e1b7eb56
jeffl <jeffl@hp.com>
2023-02-22 23:08:00 -0800

    launch defaults

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,8 @@
 	/**
 	 * @type {string | any[]}
 	 */
-	let titles = []; // = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5', ]
+	// let titles = []; // = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5', ]
+	let titles =  ['Title 1', 'Title 2', 'Title 3', 'Title 4', 'Title 5', ]
 	/**
 	 * @type {string}
 	 */
@@ -20,7 +21,7 @@
 	/**
 	 * @type {number}
 	 */
-	let questions;
+	let questions = 5;
 	/**
 	 * @type {number}
 	 */
